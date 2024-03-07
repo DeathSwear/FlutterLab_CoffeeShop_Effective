b3e7c3bdb9f083766507278cad7c67dcc178feec
DeathSwear <vlad0septa@yandex.ru>
2024-03-07 23:08:59 +0600

    made the title of the picture clearer

--- lib/src/features/menu/data/data_example.dart
+++ lib/src/features/menu/data/data_example.dart
@@ -6,27 +6,27 @@ class DataExample{
   static List<CardModel> cards_db_example =
       [
         const CardModel(
-            ico: 'lib/src/assets/images/rrr.png',
+            ico: 'lib/src/assets/images/example_image.png',
             name: 'Айс Кофе',
             price: '100 руб'
         ),
         const CardModel(
-            ico: 'lib/src/assets/images/rrr.png',
+            ico: 'lib/src/assets/images/example_image.png',
             name: 'Айс Кофе2',
             price: '120 руб'
         ),
         const CardModel(
-            ico: 'lib/src/assets/images/rrr.png',
+            ico: 'lib/src/assets/images/example_image.png',
             name: 'Айс Кофе3',
             price: '130 руб'
         ),
         const CardModel(
-            ico: 'lib/src/assets/images/rrr.png',
+            ico: 'lib/src/assets/images/example_image.png',
             name: 'Айс Кофе4',
             price: '140 руб'
         ),
         const CardModel(
-            ico: 'lib/src/assets/images/rrr.png',
+            ico: 'lib/src/assets/images/example_image.png',
             name: 'Айс Кофе5',
             price: '150 руб'
         ),
